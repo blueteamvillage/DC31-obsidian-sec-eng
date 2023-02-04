5cc06181d76db2e0f1e11c5757b6c53936aa25a4
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2023-02-03 19:18:13 -0800

    added the template file for ansible/aws usage for the windows vms

---
+++ terraform/templates/tf_windows_setup.ps1
@@ -0,0 +1,12 @@
+<powershell>
+
+$password   = "${windows_admin_password}" | ConvertTo-SecureString -asPlainText -Force
+$UserAccount = Get-LocalUser -Name "administrator"
+$UserAccount | Set-LocalUser -Password $Password
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Invoke-Expression ((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
+
+</powershell>
+
+

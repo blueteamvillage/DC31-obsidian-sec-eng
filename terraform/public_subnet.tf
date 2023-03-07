ae12750b4a4f23ff6d80020b20db2b6961e29083
Ben Bornholm <thunderwagon@Bens-MacBook-Pro.local>
2023-03-06 23:37:41 -0600

    Create red team boxes

--- terraform/public_subnet.tf
+++ terraform/public_subnet.tf
@@ -196,15 +196,61 @@ resource "aws_eip" "securityonion_server_eip" {
 }
 
 ############################################ Red team boxes ############################################
+resource "aws_security_group" "red_team_servers" {
+  vpc_id      = module.vpc.vpc_id
+  description = "Cribl security group"
+  tags = {
+    Name    = "${var.PROJECT_PREFIX}_cribl_server_sg2"
+    Project = var.PROJECT_PREFIX
+  }
+}
+
+resource "aws_security_group_rule" "allow_ssh_from_teleport" {
+  type        = "ingress"
+  description = "Allow SSH from Teleport"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = [
+    "${module.teleport.private_ip_addr}/32",
+    "${module.teleport.public_ip_addr}/32",
+  ]
+  security_group_id = aws_security_group.red_team_servers.id
+}
+
+resource "aws_security_group_rule" "allow_inbound_from_nat_gateway" {
+  type        = "ingress"
+  description = "Allow SSH"
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
+  cidr_blocks = [
+    "${module.vpc.nat_public_ips[0]}/32",
+  ]
+  security_group_id = aws_security_group.red_team_servers.id
+}
+
+resource "aws_security_group_rule" "allow_inbound_from_log4j" {
+  type        = "ingress"
+  description = "Allow SSH"
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
+  cidr_blocks = [
+    "${aws_eip.vuln_log4j_webserver.public_ip}/32",
+  ]
+  security_group_id = aws_security_group.red_team_servers.id
+}
+
 resource "aws_instance" "red_team_servers" {
   for_each = var.red_team_subnet_map
 
   ami                     = var.ubunut-ami
   instance_type           = "t3.small"
-  subnet_id               = aws_subnet.public.id
-  vpc_security_group_ids  = [aws_security_group.securityonion_server_sg2.id]
+  subnet_id               = module.vpc.public_subnets[0]
+  vpc_security_group_ids  = [aws_security_group.red_team_servers.id]
   key_name                = "${var.PROJECT_PREFIX}-ssh-key"
-  private_ip              = var.red_team_subnet_map["${each.value}"]
+  private_ip              = each.value
   disable_api_termination = true
 
   metadata_options {
@@ -226,11 +272,10 @@ resource "aws_instance" "red_team_servers" {
 }
 
 resource "aws_eip" "red_team_servers_eips" {
-  depends_on = [aws_instance.red_team_servers]
-
   for_each = var.red_team_subnet_map
 
-  instance = aws_instance.red_team_servers[]
+  #aws_instance.red_team_servers["red_team_box_charlie"]
+  instance = aws_instance.red_team_servers[each.key].id
   vpc      = true
   tags = {
     Name    = "${var.PROJECT_PREFIX}_${each.key}_server_eip"

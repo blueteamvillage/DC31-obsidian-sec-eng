2052100806571b9d5befb1f61366ea83d09dd9c3
juju4 <juju4@users.noreply.github.com>
2023-03-23 23:52:02 +0000

    fix terraform reference to metrics server

--- terraform/iot_subnet.tf
+++ terraform/iot_subnet.tf
@@ -29,7 +29,7 @@ resource "aws_security_group_rule" "iot01_allow_prometheus" {
   protocol    = "tcp"
   cidr_blocks = [
     "${module.teleport.private_ip_addr}/32",
-    "${aws_instance.metrics_server.private_ip}/32"
+    "${aws_instance.metrics.private_ip}/32"
   ]
   security_group_id = aws_security_group.iot01_server.id
 }

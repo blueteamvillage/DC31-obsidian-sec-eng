014cd3ab9ba7b57b2c9a3c375a5448146c65808b
juju4 <juju4@users.noreply.github.com>
2023-04-01 21:45:35 +0000

    fix: review from tfsec

--- terraform/s3_logs.tf
+++ terraform/s3_logs.tf
@@ -1,7 +1,71 @@
 #################### Logs Archive S3 ########################
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/ignore-public-acls/
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/block-public-acls/
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/enable-bucket-encryption/
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/encryption-customer-key/
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/enable-bucket-logging/
+# https://aquasecurity.github.io/tfsec/v1.28.1/checks/aws/s3/enable-versioning/
+
+resource "aws_kms_key" "logs_s3_key" {
+  enable_key_rotation = true
+}
+
+#tfsec:ignore:aws-s3-enable-bucket-logging
+resource "aws_s3_bucket" "logs_infra" {
+  bucket = "infra-logs-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        # kms_master_key_id = "arn"
+        kms_master_key_id = aws_kms_key.logs_s3_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name    = "${var.PROJECT_PREFIX}_infra_logs"
+    Project = var.PROJECT_PREFIX
+  }
+}
+
+resource "aws_s3_bucket_acl" "infra_logs_private" {
+  bucket = aws_s3_bucket.logs_infra.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "logs_infra_block_public" {
+  bucket            = aws_s3_bucket.logs_infra.id
+  block_public_acls = true
+  # if block enabled, ignore not needed
+  # ignore_public_acls = true
+}
 
 resource "aws_s3_bucket" "logs_archive_raw" {
   bucket = "raw-logs-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "infra-logs-bucket"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        # kms_master_key_id = "arn"
+        kms_master_key_id = aws_kms_key.logs_s3_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   tags = {
     Name    = "${var.PROJECT_PREFIX}_raw_logs"
@@ -14,8 +78,33 @@ resource "aws_s3_bucket_acl" "raw_logs_private" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "logs_archive_raw_block_public" {
+  bucket            = aws_s3_bucket.logs_archive_raw.id
+  block_public_acls = true
+  # if block enabled, ignore not needed
+  # ignore_public_acls = true
+}
+
 resource "aws_s3_bucket" "logs_archive_enriched" {
   bucket = "enriched-logs-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "infra-logs-bucket"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        # kms_master_key_id = "arn"
+        kms_master_key_id = aws_kms_key.logs_s3_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   tags = {
     Name    = "${var.PROJECT_PREFIX}_enriched_logs"
@@ -26,4 +115,11 @@ resource "aws_s3_bucket" "logs_archive_enriched" {
 resource "aws_s3_bucket_acl" "enriched_logs_private" {
   bucket = aws_s3_bucket.logs_archive_enriched.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "enriched_logs_privateblock_public" {
+  bucket            = aws_s3_bucket.logs_archive_enriched.id
+  block_public_acls = true
+  # if block enabled, ignore not needed
+  # ignore_public_acls = true
 }

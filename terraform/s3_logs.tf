9031a70b6a632d11cb48c59bfea245a0d7dfce80
juju4 <juju4@users.noreply.github.com>
2023-04-01 21:56:33 +0000

    fix: review from tfsec (2)

--- terraform/s3_logs.tf
+++ terraform/s3_logs.tf
@@ -40,10 +40,11 @@ resource "aws_s3_bucket_acl" "infra_logs_private" {
 }
 
 resource "aws_s3_bucket_public_access_block" "logs_infra_block_public" {
-  bucket            = aws_s3_bucket.logs_infra.id
-  block_public_acls = true
-  # if block enabled, ignore not needed
-  # ignore_public_acls = true
+  bucket                  = aws_s3_bucket.logs_infra.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket" "logs_archive_raw" {
@@ -79,10 +80,11 @@ resource "aws_s3_bucket_acl" "raw_logs_private" {
 }
 
 resource "aws_s3_bucket_public_access_block" "logs_archive_raw_block_public" {
-  bucket            = aws_s3_bucket.logs_archive_raw.id
-  block_public_acls = true
-  # if block enabled, ignore not needed
-  # ignore_public_acls = true
+  bucket                  = aws_s3_bucket.logs_archive_raw.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket" "logs_archive_enriched" {
@@ -118,8 +120,9 @@ resource "aws_s3_bucket_acl" "enriched_logs_private" {
 }
 
 resource "aws_s3_bucket_public_access_block" "enriched_logs_privateblock_public" {
-  bucket            = aws_s3_bucket.logs_archive_enriched.id
-  block_public_acls = true
-  # if block enabled, ignore not needed
-  # ignore_public_acls = true
+  bucket                  = aws_s3_bucket.logs_archive_enriched.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
